812af0230fafcacf4c020ca9e73aae6713ebb6d5
Momen <43503904+MomenDarziV1L@users.noreply.github.com>
2020-04-17 02:21:35 +0200

    Delete loginInfoAlert.js

--- loginInfoAlert.js
+++
@@ -1,8 +0,0 @@
-function loginInfoAlert( ){
-    // Select the input id and get its value 
-    var logUsername = document.getElementById("username").value;
-    var logPassword = document.getElementById("password").value;
-
-    // Display the values
-    alert(logUsername + ", " + logPassword);
-}
